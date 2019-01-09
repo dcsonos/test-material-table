ede59ddb6687c08e93628ba9a9b9deb10506083f
Diego Da Costa Souza <dcsonos@gmail.com>
2019-01-09 14:27:09 +0100

    FIX E2E Tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo()
-    expect(page.getTitleText()).toEqual('Welcome to test-material-table!')
+    expect(page.getTitleText()).toEqual('Sample table with device list')
   })
 })

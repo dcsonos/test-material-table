a52863f8954ecd9da5dcf269037b31d536f2d273
Diego Da Costa Souza <dcsonos@gmail.com>
2019-01-10 08:35:41 +0100

    ADD Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { DeviceService } from './device/device.service'
 import { SelectedDeviceComponent } from './selected-device/selected-device.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent, SelectedDeviceComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [DeviceService],
   bootstrap: [AppComponent],
 })
